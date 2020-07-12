ff70074aec0de57749738e52227cc5c65508ea54
AHMED AFANDY <eng.computer1@yahoo.com>
2020-07-12 19:30:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200712192804_add_passenger_id_to_rides.rb
+++ db/migrate/20200712192804_add_passenger_id_to_rides.rb
@@ -1,5 +1,5 @@
 class AddPassengerIdToRaides < ActiveRecord::Migration[5.0]
   def change
-    add_column :raides, :passenger_id, :integer
+    add_column :rides, :passenger_id, :integer
   end
 end
